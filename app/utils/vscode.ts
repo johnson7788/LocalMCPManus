53b9a26ececa3dc8d3cf6aae20013ad390c78829
johnson <johnson@macbook.com>
2025-03-23 23:08:57 +0800

    2025-03-23_23:08

--- app/utils/vscode.ts
+++ app/utils/vscode.ts
@@ -11,14 +11,14 @@ class VSCodeAPIWrapper {
    * @param message Abitrary data (must be JSON serializable) to send to the extension context.
    */
   public async postMessage(message: WebviewMessage) {
-    console.log('postMessage: 存储 Code API 收到消息:', message);
+    console.log('postMessage: 更新一条配置信息:', message);
     // 修复参数结构（原参数嵌套了不必要的key字段）
     await window.api.invoke('append-message', message);
     console.log(message);
   }
 
   public async getState(): Promise<unknown> {
-    console.log('getState: 存储 Code API 收到消息');
+    console.log('getState: 获取配置信息');
     return window.api.invoke('get-config-data'); // 移除冗余参数
   }
 
